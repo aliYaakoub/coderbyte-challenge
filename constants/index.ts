c136418a98575ea742e83fcf0edc3e236fc4671b
aliYaakoub <ali.yaakoub.2021@gmail.com>
2024-01-20 20:40:53 +0200

    changing the dataset a little

--- constants/index.ts
+++ constants/index.ts
@@ -1,14 +1,13 @@
 export const data = [
   {
-    title: 'Lorem, Grid ipsum dolor sit amet consectetur adipisicing elit.',
+    title: 'This is the first blog post title.',
     description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tenetur eius nisi doloremque temporibus totam quas accusantium, iusto et non consectetur nobis explicabo repudiandae ad laboriosam facere assumenda vero at.',
+      'This is the body of the blog, This is the body of the blog, This is the body of the blog, This is the body of the blog, This is the body of the blog, This is the body of the blog, This is the body of the blog.',
     date: 'Oct 9, 2022',
   },
   {
-    title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tenetur Alloush eius nisi doloremque temporibus totam quas accusantium, iusto et non consectetur nobis explicabo repudiandae ad laboriosam facere assumenda vero at.',
+    title: 'This is another blog post, And this is the title.',
+    description: 'And this is the body of the other blog post.',
     date: 'Oct 9, 2022',
   },
   {
